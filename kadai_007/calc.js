51fa3bab989b71d2e6605b1b242d3c74eaf4128a
smaruyama2009 <151481610+smaruyama2009@users.noreply.github.com>
2024-01-28 10:06:14 +0900

    kadai_007をアップロード

---
+++ kadai_007/calc.js
@@ -0,0 +1,15 @@
+// 変数numに値を代入
+ num = 30
+
+ if (num % 3 === 0 && num % 5 === 0) {
+  console.log('３と５の倍数です')
+ }
+ else if (num % 3 === 0) {
+  console.log('３の倍数です')
+ } 
+ else if (num % 5 === 0) {
+  console.log('5の倍数です')
+ }
+ else {
+  console.log(num)
+ }
